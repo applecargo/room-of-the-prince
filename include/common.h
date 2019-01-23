edc49c055ae3118635ddaad6f630884d959cf1d9
Dooho Yi <pilotedeguerre@gmail.com>
2019-01-24 00:33:18 +0900

    organizing pio env.

---
+++ include/common.h
@@ -0,0 +1,40 @@
+#pragma once
+
+// member identity
+#define ID_MOTION_SENSOR (0x1D00 + 1)
+#define ID_RELAY_CTRLER  (0x1D00 + 2)
+#define ID_POINT_MOTOR   (0x1D00 + 3)
+//(choice)
+#define IDENTITY ID_MOTION_SENSOR
+
+// board
+#define BOARD_NODEMCU_ESP12E (0xBD00 + 1)
+#define BOARD_NODEMCU_ESP12N (0xBD00 + 2)
+#define BOARD_NODEMCU_ESP32  (0xBD00 + 3)
+//(choice)
+#define BOARD_SELECT BOARD_NODEMCU_ESP12E
+
+// mesh
+#define MESH_SSID "room-of-the-prince"
+#define MESH_PASSWORD "12*1000/128"
+#define MESH_PORT 5555
+#define MESH_CHANNEL 5
+// #define MESH_ANCHOR
+
+//
+// LED status indication
+//   operation modes
+//     0 - booted. and running. no connection. scanning.
+//     1 - + connected.
+//   notifying patterns
+//     0 - steady on
+//     1 - slow blinking (syncronized)
+//
+#if (BOARD_SELECT==BOARD_NODEMCU_ESP12E)
+#define LED_PIN 2 // built-in LED
+#endif
+#define LED_PERIOD (1000+(12*1000/128))
+#define LED_ONTIME (12*1000/128)
+
+// event handlers fo connection-related events
+void gotMessageCallback(uint32_t from, String & msg);
--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,16 @@
 #include <Arduino.h>
 
-/*
- * freenode devices to the mesh (part1)
- *
- *   - a I2C Master that logs in to the mesh.
- *
- *   - soundmesh project @ 2018. 04 ~ 06
- */
+// common sense
+#include "common.h"
 
-#define BOARD_NODEMCU_ESP12E 1
-#define BOARD_NODEMCU_ESP12N 2
-#define BOARD_NODEMCU_ESP32  3
-
-/*
- * Board
- *
- * ESP8266 (nodemcu-esp12e) status ==> Stable.
- *
- * ESP8266 (esp12n) status ==> Stable.
- * Carefully select proper compiler/uploader setup @ arduino
- *
- * ESP32 status ==> Unstable.
- * problem: i2c stabillity. + (mesh stability)
- * trial #1: try stickbreaker's arduino-esp32 which is seriously targeting i2c stability
- * trial #2: try encode/decode command bytes. to be single byte. seems RTOS running under the hood (hal) causing problem with i2c strict timing. so i feel good on single bytes according to people's talks.
- * first thing first.. : confirm pinout. + stickbreaker (if not work/or unstable) + encode/decode in 1 byte. is my best effort. then we might have to consider different way for communication... sadly.
- * a goooood article on esp32 subject.. possibilities.. potentials..!
- *  --> https://mjrobot.org/iot-made-simple-playing-with-the-esp32-on-arduino-ide/
- * pinout confirmation
- * SDA ==> GPIO 21
- * SCL ==> GPIO 22
- * teensy 3.5 default
- * SDA ==> 18
- * SCL ==> 19
- * so,
- * esp32 ==> teensy 3.5
- *  21 <==> 18
- *  22 <==> 19
- * and!
- * NOTE: esp32 i2c function will drive bus pull-up on the fly! Don't use 3.3k pullups at teensy side! otherwise it won't work.
- */
-#define BOARD_SELECT BOARD_NODEMCU_ESP12E
-
-/*
- * Network
- *
- * define or undef MESH_ANCHOR : let this node be a 'root'-inclusion obsessive or simply free.
- * anchors should be placed around 'root' to speed up mesh establishment and stabilize mesh integrity.
- */
-#define MESH_SSID "soundmesh"
-#define MESH_PASSWORD "timemachine999"
-#define MESH_PORT 5555
-#define MESH_CHANNEL 5
-// #define MESH_ANCHOR
-
-// shared global (protocol) : begin
-//command (i2c)
-#define CMD_LENGTH 10
-#define CMD_BUFF_LEN (CMD_LENGTH + 1)
-char cmdstr[CMD_BUFF_LEN] = "NONE#SSS@P";
-//info (mesh)
-char memberCount = -1;      //before any update from the mesh
-//flags (events)
-bool cmdsent = false;
-bool newcmd = false;
-// shared global (protocol) : end
-
-//libraries
-#include <Wire.h>
+// painless mesh
 #include <painlessMesh.h>
-
-//i2c
-const int i2c_addr = 3;
-
-//mesh
 painlessMesh mesh;
-void receivedCallback(uint32_t from, String & msg) { // needed to be exist
-  //i2c
-  msg.toCharArray(cmdstr, CMD_BUFF_LEN);
 
-  Wire.beginTransmission(i2c_addr);
-  int nb = Wire.write(cmdstr);
-  Wire.endTransmission();
-}
-
-//tasks
+//scheduler
 Scheduler runner;
-//
-// LED status indication
-//
-// operation modes
-//
-// 0 - booted. and running. no connection. scanning.
-// 1 - + connected.
-//
-// notifying patterns
-//
-// 0 - steady on
-// 1 - slow blinking (syncronized)
-//
-#define LED_PIN 2 // built-in LED
-#define LED_PERIOD 1000
-#define LED_ONTIME 100
+
+//task #0 : connection indicator
 bool onFlag = false;
 //prototypes
 void taskStatusBlink_steadyOn();
@@ -133,6 +42,11 @@ void taskStatusBlink_slowblink_insync() {
 void taskStatusBlink_fastblink() {
 }
 
+// mesh callbacks
+void receivedCallback(uint32_t from, String & msg) { // REQUIRED
+  // let the device know.
+  gotMessageCallback(from, msg);
+}
 void changedConnectionCallback() {
   // Serial.println("changed connection");
   // Serial.print("mesh.getNodeList().size():");
@@ -152,12 +66,14 @@ void changedConnectionCallback() {
     statusblinks.enable();
     // Serial.println("disconnected!");
   }
+  // let the device know.
+  changedSeatCallback();
+}
+void newConnectionCallback(uint32_t nodeId) {
+  changedConnectionCallback();
 }
 
 void setup() {
-  //i2c master
-  Wire.begin();
-
   //led
   pinMode(LED_PIN, OUTPUT);
 
@@ -169,6 +85,7 @@ void setup() {
 #endif
   //callbacks
   mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
 
   //tasks
